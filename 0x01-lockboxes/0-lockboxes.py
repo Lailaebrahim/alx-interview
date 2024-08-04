d6e95941d4b1e9a3041753f8c74eae6f7f7fa704
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-05 01:07:30 +0300

    Solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
+"""_summary_ module to define method canUnlockAll
 """
-Summary module to define method can_unlock_all.
-"""
 
-def can_unlock_all(boxes):
-  """
-  Determines if all the boxes in the list can be unlocked.
-  Args:
-    boxes (list): A list of lists representing the boxes and their corresponding keys.
-  Returns:
-    bool: True if all the boxes can be unlocked, False otherwise.
-  """
-  unlocked = [False] * len(boxes)
-  unlocked[0] = True
-  keys = [0]  # Start with the keys from the first box
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes in the list can be unlocked.
+    Args:
+      boxes (list): A list of lists representing the boxes and their corresponding keys.
+    Returns:
+      bool: True if all the boxes can be unlocked, False otherwise.
+    """
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+    keys = [0]  # Start with the keys from the first box
 
-  while keys:
-    current_key = keys.pop()
-    for new_key in boxes[current_key]:
-      if new_key < len(boxes) and not unlocked[new_key]:
-        unlocked[new_key] = True
-        keys.append(new_key)
+    while keys:
+        current_key = keys.pop()
+        for new_key in boxes[current_key]:
+            if new_key < len(boxes) and not unlocked[new_key]:
+                unlocked[new_key] = True
+                keys.append(new_key)
 
-  return all(unlocked)
+    return all(unlocked)
