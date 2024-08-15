3e24493aa570854399ddae6ce2b3b4474ea5a6fd
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-15 22:13:58 +0300

    trail 2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -29,28 +29,37 @@ accumulated_data = {"File size": 0, "200": 0, "301": 0,
                     "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 counter = 0
 
-for line in sys.stdin:
-    # Remove trailing newline
-    line = line.strip()
-    # Process the line
-    if counter <= 10:
+try:
+    for line in sys.stdin:
+        # Remove trailing newline
+        line = line.strip()
+        # Process the line
+        counter = 0
+        if re.match(r"\d+\.\d+\.\d+\.\d+ - \[.*\] \
+          \"GET /projects/260 HTTP/1.1\" \d+ \d+", line):
+            data = line.split()
+            if len(data) > 2:
+                counter += 1
+                status = data[-2]
+                file_size = data[-1]
+                accumulated_data["File size"] += int(file_size)
+                if status in accumulated_data:
+                    accumulated_data[status] += 1
+        else:
+            continue
+        
+        if counter == 10:
+            print("File size: {}".format(accumulated_data["File size"]))
+            for key, value in accumulated_data.items():
+                if key != "File size" and value != 0:
+                    print("{}: {}".format(key, value))
+            counter = 0
+            accumulated_data = {"File size": 0, "200": 0, "301": 0,
+                                "400": 0, "401": 0, "403": 0,
+                                "404": 0, "405": 0, "500": 0}
+finally:
         print("File size: {}".format(accumulated_data["File size"]))
         for key, value in accumulated_data.items():
             if key != "File size" and value != 0:
                 print("{}: {}".format(key, value))
-    accumulated_data = {"File size": 0, "200": 0, "301": 0,
-                        "400": 0, "401": 0, "403": 0,
-                        "404": 0, "405": 0, "500": 0}
-    counter = 0
-    if re.match(r"\d+\.\d+\.\d+\.\d+ - \[.*\] \
-      \"GET /projects/260 HTTP/1.1\" \d+ \d+", line):
-        data = line.split()
-        if len(data) > 1:
-            status = data[-2]
-            file_size = data[-1]
-            accumulated_data["File size"] += int(file_size)
-            if status in accumulated_data:
-                accumulated_data[status] += 1
-    else:
-        continue
-    counter += 1
+        sys.exit(0)
