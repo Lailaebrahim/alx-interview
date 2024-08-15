18da2e6c29bcb28856d74c51af749d6546310c60
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-15 21:17:49 +0300

    trail 1

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,11 +5,19 @@ import re
 
 accumulated_data = {"File size": 0, "200": 0, "301": 0,
                     "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+counter = 0
+
 for line in sys.stdin:
     # Remove trailing newline
     line = line.strip()
-
     # Process the line
+    if counter % 10 == 0:
+        print("File size: {}".format(accumulated_data["File size"]))
+        for key, value in accumulated_data.items():
+            if key != "File size" and value != 0:
+                print("{}: {}".format(key, value))
+    accumulated_data = {"File size": 0, "200": 0, "301": 0,
+                        "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
     if re.match(r"\d+\.\d+\.\d+\.\d+ - \[.*\] \"GET /projects/260 HTTP/1.1\" \d+ \d+", line):
         data = line.split()
         if len(data) > 1:
@@ -20,3 +28,4 @@ for line in sys.stdin:
                 accumulated_data[status] += 1
     else:
         continue
+    counter += 1
