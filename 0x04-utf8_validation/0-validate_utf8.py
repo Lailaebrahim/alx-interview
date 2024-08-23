b1a073416739d1940f75f59ba1708ace5fefa221
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-23 22:38:49 +0300

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python3
-"""_summary_  a method that determines if a given data set represents a valid UTF-8 encoding.
 """
+A method that determines if a given data set represents a valid UTF-8 encoding.
+"""
+
 def validUTF8(data):
-    """_summary_
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
 
     Args:
-        data (list of intgers): The data will be represented by a list of integers
+        data (list of int): The data will be represented by a list of integers.
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else False.
     """
-    for i in range(len(data)):
+    i = 0
+    while i < len(data):
         num = data[i] & 255
         item_b = bin(num)[2:].zfill(8)
         leading_ones = len(item_b) - len(item_b.lstrip('1'))
+        
         if leading_ones == 0:
+            i += 1
             continue
+        
         if leading_ones == 1 or leading_ones > 4 or i + leading_ones > len(data):
             return False
         
-        for j in range(leading_ones - 1):
-            k = i + j + 1
+        for j in range(1, leading_ones):
+            k = i + j
             item_b = bin(data[k])[2:].zfill(8)
             if not item_b.startswith('10'):
                 return False
+        
+        i += leading_ones
+    
     return True
+
+
+print(validUTF8([240, 188, 128, 167]))
