6fabddfe4735a2f5b2f73ca3a52ee525d3e159d9
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-23 22:08:07 +0300

    Task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""_summary_  a method that determines if a given data set represents a valid UTF-8 encoding.
+"""
+def validUTF8(data):
+    """_summary_
+
+    Args:
+        data (list of intgers): The data will be represented by a list of integers
+    """
+    for i in range(len(data) - 1):
+        if data[i] > 255 or data[i] < 0:
+            return False
+        item_b = bin(data[i])[2:].zfill(8)
+        leading_ones = len(item_b) - len(item_b.lstrip('1'))
+        if leading_ones == 0:
+            continue
+        if leading_ones == 1 or leading_ones > 4 or i + leading_ones > len(data):
+            return False
+        
+        for j in range(leading_ones - 1):
+            k = i + j + 1
+            item_b = bin(data[k])[2:].zfill(8)
+            if not item_b.startswith('10'):
+                return False
+    return True
