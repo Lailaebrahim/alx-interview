6c0515de3a1ae88e9d4186718ebd2122a4776c2f
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-23 23:43:12 +0300

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,17 +16,21 @@ def validUTF8(data):
     """
     i = 0
     while i < len(data):
+        # to handle the 8 least significant bits of each integer
         num = data[i] & 255
+        # to get the number of leading ones in the binary representation
         item_b = bin(num)[2:].zfill(8)
         lead_ones = len(item_b) - len(item_b.lstrip('1'))
-
+        # if no leading ones then it's a valid single-byte character.
         if lead_ones == 0:
             i += 1
             continue
-
+        # if only one leading one and not a continuation byte  
+        # or leading ones  more than 4 it'a an invalid
         if lead_ones == 1 or lead_ones > 4 or i + lead_ones > len(data):
             return False
 
+        # check the next items for Continuation Bytes
         for j in range(1, lead_ones):
             k = i + j
             item_b = bin(data[k])[2:].zfill(8)
