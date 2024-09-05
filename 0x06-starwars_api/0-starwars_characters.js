4a82934ff2fd1375b58449e931d1f3eaf6e88fef
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-06 02:58:48 +0300

    Star Wars Api chanllenge solution

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -11,16 +11,16 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, async function (error, response, body) {
   if (error) {
-    console.log(error);
+    print(error);
   } else {
     const characters = JSON.parse(body).characters;
     for (const character of characters) {
       await new Promise((resolve, reject) => {
         request(character, function (error, response, body) {
           if (error) {
-            console.log(error);
+            print(error);
           } else {
-            console.log(JSON.parse(body).name);
+            print(JSON.parse(body).name);
           }
           resolve();
         });
