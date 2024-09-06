28c28e41411a80fbcf657f8119c2c1065ad2ef12
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-06 03:10:51 +0300

    Star Wars Api chanllenge solution

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,26 @@
 #!/usr/bin/node
 /*
-    Write a script that prints all characters of a Star Wars movie:
+    Write a script that console.logs all characters of a Star Wars movie:
     1- base url = https://swapi-api.alx-tools.com/api/
     2- then use first argumrnt to get the required film url=https://swapi-api.alx-tools.com/api/films/arg1
-    3- get chracters key of the json of prev request
+    3- get value of characters key of the json of prev request
     3- iterate over the characters's value list and send a get request to each item
-    4- get the value of the key name for each request to a chracter url
+    4- get the value of the key name for each request to a character url
  */
 const request = require('request');
-var characters_names = [];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, async function (error, response, body) {
   if (error) {
-    print(error);
+    console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
     for (const character of characters) {
       await new Promise((resolve, reject) => {
         request(character, function (error, response, body) {
           if (error) {
-            print(error);
+            console.log(error);
           } else {
-            characters_names.push(JSON.parse(body).name);
+            console.log(JSON.parse(body).name);
           }
           resolve();
         });
@@ -29,7 +28,3 @@ request(url, async function (error, response, body) {
     }
   }
 });
-var i = 0;
-for (i = 0; i < characters_names.length; i++){
-    print(characters_names[i]);
-}
